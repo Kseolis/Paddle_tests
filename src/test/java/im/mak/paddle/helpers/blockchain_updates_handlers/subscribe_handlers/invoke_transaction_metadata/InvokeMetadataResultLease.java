c844dad653dc4cf74c87bca9978c003cff54f632
vnikolaenko <vnikolaenko@web3tech.ru>
2022-08-04 13:39:05 +0300

    added Lease Metadata Handler

---
+++ src/test/java/im/mak/paddle/helpers/blockchain_updates_handlers/subscribe_handlers/invoke_transaction_metadata/InvokeMetadataResultLease.java
@@ -0,0 +1,30 @@
+package im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.invoke_transaction_metadata;
+
+import com.wavesplatform.crypto.base.Base58;
+import com.wavesplatform.protobuf.transaction.InvokeScriptResultOuterClass.InvokeScriptResult.Lease;
+
+public class InvokeMetadataResultLease extends BaseInvokeMetadata {
+    public static String getInvokeMetadataLeasesRecipientPublicKey(int metadataIndex, int argIndex) {
+        return Base58.encode(getInvokeMetadataLeases(metadataIndex, argIndex)
+                .getRecipient()
+                .getPublicKeyHash()
+                .toByteArray()
+        );
+    }
+
+    public static long getInvokeMetadataLeasesAmount(int metadataIndex, int argIndex) {
+        return getInvokeMetadataLeases(metadataIndex, argIndex).getAmount();
+    }
+
+    public static String getInvokeMetadataLeaseId(int metadataIndex, int argIndex) {
+        return Base58.encode(getInvokeMetadataLeases(metadataIndex, argIndex)
+                .getLeaseId()
+                .toByteArray()
+        );
+    }
+
+    private static Lease getInvokeMetadataLeases(int metadataIndex, int argIndex) {
+        return getInvokeScriptResult(metadataIndex)
+                .getLeases(argIndex);
+    }
+}

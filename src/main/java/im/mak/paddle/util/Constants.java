dc7909a64ab50975ed7f7b703bb1ae8bcdb63927
vnikolaenko <vnikolaenko@web3tech.ru>
2022-08-05 17:15:23 +0300

    added arg constants

--- src/main/java/im/mak/paddle/util/Constants.java
+++ src/main/java/im/mak/paddle/util/Constants.java
@@ -44,7 +44,6 @@ public class Constants {
     public static final String INACTIVE_STATUS_LEASE = "INACTIVE";
 
     // asset data keys
-
     public static final String ASSET_ID = "asset_id";
     public static final String ISSUER = "issuer";
     public static final String DECIMALS = "decimals";
@@ -52,4 +51,10 @@ public class Constants {
     public static final String NAME = "name";
     public static final String REISSUE = "reissue";
     public static final String VOLUME = "volume";
+
+    // arg types
+    public static final String INTEGER = "integer";
+    public static final String STRING = "string";
+    public static final String BOOLEAN = "boolean";
+    public static final String BINARY = "binary_value";
 }

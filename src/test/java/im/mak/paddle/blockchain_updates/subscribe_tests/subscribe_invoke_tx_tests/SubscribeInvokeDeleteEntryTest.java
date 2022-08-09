16f5be67c13a0e4328fd4b0a944b6084bf7439b9
vnikolaenko <vnikolaenko@web3tech.ru>
2022-08-09 12:29:49 +0300

    added assertionsCheck

--- src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/subscribe_invoke_tx_tests/SubscribeInvokeDeleteEntryTest.java
+++ src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/subscribe_invoke_tx_tests/SubscribeInvokeDeleteEntryTest.java
@@ -5,20 +5,27 @@ import org.junit.jupiter.api.Test;
 
 import static com.wavesplatform.transactions.InvokeScriptTransaction.LATEST_VERSION;
 import static im.mak.paddle.Node.node;
+import static im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_tx_tests.invoke_transactions_checkers.InvokeMetadataAssertions.*;
+import static im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_tx_tests.invoke_transactions_checkers.InvokeMetadataAssertions.checkDataMetadata;
+import static im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_tx_tests.invoke_transactions_checkers.InvokeStateUpdateAssertions.checkStateUpdateBalance;
+import static im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_tx_tests.invoke_transactions_checkers.InvokeStateUpdateAssertions.checkStateUpdateDataEntries;
 import static im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_tx_tests.invoke_transactions_checkers.InvokeTransactionAssertions.checkInvokeSubscribeTransaction;
+import static im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_tx_tests.invoke_transactions_checkers.InvokeTransactionAssertions.checkPaymentsSubscribe;
 import static im.mak.paddle.helpers.PrepareInvokeTestsData.*;
 import static im.mak.paddle.helpers.PrepareInvokeTestsData.getAssetId;
-import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.SubscribeHandler.getAppend;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.SubscribeHandler.subscribeResponseHandler;
 import static im.mak.paddle.helpers.transaction_senders.BaseTransactionSender.setVersion;
-import static im.mak.paddle.helpers.transaction_senders.invoke.InvokeCalculationsBalancesAfterTransaction.balancesAfterPaymentInvoke;
+import static im.mak.paddle.helpers.transaction_senders.invoke.InvokeCalculationsBalancesAfterTransaction.*;
 import static im.mak.paddle.helpers.transaction_senders.invoke.InvokeScriptTransactionSender.invokeSenderWithPayment;
-import static im.mak.paddle.util.Constants.SUM_FEE;
+import static im.mak.paddle.util.Constants.*;
+import static im.mak.paddle.util.Constants.WAVES_STRING_ID;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class SubscribeInvokeDeleteEntryTest extends InvokeBaseTest {
     @Test
     @DisplayName("subscribe invoke with DeleteEntry")
     void subscribeInvokeWithDeleteEntry() {
+        String intValueAfter = String.valueOf(0);
         getTestsData().prepareDataForDeleteEntryTests();
 
         setVersion(LATEST_VERSION);
@@ -28,8 +35,34 @@ public class SubscribeInvokeDeleteEntryTest extends InvokeBaseTest {
         height = node().getHeight();
         subscribeResponseHandler(channel, getDAppAccount(), height, height);
         prepareInvoke(getDAppAccount());
-        System.out.println(getAppend());
 
-        checkInvokeSubscribeTransaction(SUM_FEE);
+        assertionsCheck(getWavesAmount().value(), String.valueOf(getIntArg()), intValueAfter);
+    }
+
+    private void assertionsCheck(long payment, String intVal, String valAfter) {
+        assertAll(
+                () -> checkInvokeSubscribeTransaction(getFee()),
+                () -> checkPaymentsSubscribe(payment),
+
+                () -> checkMainMetadata(0),
+                () -> checkArgumentsMetadata(0, 0, INTEGER, intVal),
+                () -> checkPaymentMetadata(0, 0, payment),
+                () -> checkDataMetadata(0, 0, INTEGER, DATA_ENTRY_INT, intVal),
+
+                () -> checkStateUpdateBalance(0,
+                        0,
+                        getCallerAddress(),
+                        WAVES_STRING_ID,
+                        getCallerBalanceWavesBeforeTransaction(), getCallerBalanceWavesAfterTransaction()),
+
+                () -> checkStateUpdateBalance(0,
+                        1,
+                        getDAppAccountAddress(),
+                        "",
+                        getDAppBalanceWavesBeforeTransaction(), getDAppBalanceWavesAfterTransaction()),
+
+
+                () -> checkStateUpdateDataEntries(0, 0, getDAppAccountAddress(), DATA_ENTRY_INT, valAfter)
+        );
     }
 }

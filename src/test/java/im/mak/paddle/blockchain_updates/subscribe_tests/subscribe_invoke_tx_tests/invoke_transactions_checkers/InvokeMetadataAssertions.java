d71d43bc106bd6b061e9ced7887d39bb1276ff53
vnikolaenko <vnikolaenko@web3tech.ru>
2022-08-09 11:09:50 +0300

    added method checkDataMetadata

--- src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/subscribe_invoke_tx_tests/invoke_transactions_checkers/InvokeMetadataAssertions.java
+++ src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/subscribe_invoke_tx_tests/invoke_transactions_checkers/InvokeMetadataAssertions.java
@@ -1,5 +1,7 @@
 package im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_tx_tests.invoke_transactions_checkers;
 
+import org.bouncycastle.util.encoders.Base64;
+
 import static im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_tx_tests.InvokeBaseTest.getDAppAccountAddress;
 import static im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_tx_tests.InvokeBaseTest.getDAppFunctionName;
 import static im.mak.paddle.helpers.ConstructorRideFunctions.*;
@@ -10,6 +12,7 @@ import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handle
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.invoke_transaction_metadata.InvokeMetadataPayment.getInvokeMetadataPaymentsAmount;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.invoke_transaction_metadata.InvokeMetadataResultBurn.getInvokeMetadataResultBurnAmount;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.invoke_transaction_metadata.InvokeMetadataResultBurn.getInvokeMetadataResultBurnAssetId;
+import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.invoke_transaction_metadata.InvokeMetadataResultData.*;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.invoke_transaction_metadata.InvokeMetadataResultIssue.*;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.invoke_transaction_metadata.InvokeMetadataResultIssue.getInvokeMetadataResultIssueNonce;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.invoke_transaction_metadata.InvokeMetadataResultLease.getInvokeMetadataLeasesAmount;
@@ -31,8 +34,11 @@ public class InvokeMetadataAssertions {
 
     public static void checkArgumentsMetadata(int metadataIndex, int dataIndex, String argType, String argValue) {
         switch (argType) {
-            case BINARY:
-                assertThat(getInvokeMetadataArgBinaryValue(metadataIndex, dataIndex)).isEqualTo(argValue);
+            case BINARY_BASE58:
+                assertThat(getInvokeMetadataArgBinaryValueBase58(metadataIndex, dataIndex)).isEqualTo(argValue);
+                break;
+            case BINARY_BASE64:
+                assertThat(getInvokeMetadataArgBinaryValueBase64(metadataIndex, dataIndex)).isEqualTo(argValue);
                 break;
             case INTEGER:
                 assertThat(getInvokeMetadataArgIntegerValue(metadataIndex, dataIndex)).isEqualTo(argValue);
@@ -92,6 +98,33 @@ public class InvokeMetadataAssertions {
                 () -> assertThat(getInvokeMetadataResultReissueAmount(metadataIndex, dataIndex)).isEqualTo(amount),
                 () -> assertThat(getInvokeMetadataResultReissueIsReissuable(metadataIndex, dataIndex)).isEqualTo(reissue)
         );
+    }
+
+    public static void checkDataMetadata(int metadataIndex, int dataIndex, String type, String key, String argValue) {
+        switch (type) {
+            case BINARY_BASE58:
+                assertThat(getInvokeMetadataResultDataKey(metadataIndex, dataIndex)).isEqualTo(key);
+                assertThat(getInvokeMetadataResultDataBinaryBase58Value(metadataIndex, dataIndex)).isEqualTo(argValue);
+                break;
+            case BINARY_BASE64:
+                assertThat(getInvokeMetadataResultDataKey(metadataIndex, dataIndex)).isEqualTo(key);
+                assertThat(getInvokeMetadataResultDataBinaryBase64Value(metadataIndex, dataIndex)).isEqualTo(argValue);
+                break;
+            case INTEGER:
+                assertThat(getInvokeMetadataResultDataKey(metadataIndex, dataIndex)).isEqualTo(key);
+                assertThat(getInvokeMetadataResultDataIntegerValue(metadataIndex, dataIndex)).isEqualTo(argValue);
+                break;
+            case STRING:
+                assertThat(getInvokeMetadataResultDataKey(metadataIndex, dataIndex)).isEqualTo(key);
+                assertThat(getInvokeMetadataResultDataStringValue(metadataIndex, dataIndex)).isEqualTo(argValue);
+                break;
+            case BOOLEAN:
+                assertThat(getInvokeMetadataResultDataKey(metadataIndex, dataIndex)).isEqualTo(key);
+                assertThat(getInvokeMetadataResultDataBoolValue(metadataIndex, dataIndex)).isEqualTo(argValue);
+                break;
+            default:
+                break;
+        }
     }
 
     public static void checkLeaseMetadata(int metadataIndex, int dataIndex, String publicKeyHash, long amount) {

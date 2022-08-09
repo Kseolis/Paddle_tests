1623c72aa8a26f6e40138c67120adedffa183fb9
vnikolaenko <vnikolaenko@web3tech.ru>
2022-08-09 11:09:27 +0300

    added getInvokeMetadataArgBinaryValueBase64

--- src/test/java/im/mak/paddle/helpers/blockchain_updates_handlers/subscribe_handlers/invoke_transaction_metadata/InvokeMetadataArgs.java
+++ src/test/java/im/mak/paddle/helpers/blockchain_updates_handlers/subscribe_handlers/invoke_transaction_metadata/InvokeMetadataArgs.java
@@ -1,14 +1,19 @@
 package im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.invoke_transaction_metadata;
 
 import com.wavesplatform.crypto.base.Base58;
+import com.wavesplatform.crypto.base.Base64;
 
 public class InvokeMetadataArgs extends BaseInvokeMetadata {
     public static String getInvokeMetadataArgIntegerValue(int metadataIndex, int argIndex) {
         return String.valueOf(getInvokeMetadata(metadataIndex).getArguments(argIndex).getIntegerValue());
     }
 
-    public static String getInvokeMetadataArgBinaryValue(int metadataIndex, int argIndex) {
+    public static String getInvokeMetadataArgBinaryValueBase58(int metadataIndex, int argIndex) {
         return Base58.encode(getInvokeMetadata(metadataIndex).getArguments(argIndex).getBinaryValue().toByteArray());
+    }
+
+    public static String getInvokeMetadataArgBinaryValueBase64(int metadataIndex, int argIndex) {
+        return Base64.encode(getInvokeMetadata(metadataIndex).getArguments(argIndex).getBinaryValue().toByteArray());
     }
 
     public static String getInvokeMetadataArgStringValue(int metadataIndex, int argIndex) {

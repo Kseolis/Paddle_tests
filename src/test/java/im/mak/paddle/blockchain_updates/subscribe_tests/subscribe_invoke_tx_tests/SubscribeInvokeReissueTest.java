516f428fde5c42f00324494b6d0217becc9c5484
vnikolaenko <vnikolaenko@web3tech.ru>
2022-08-15 10:12:07 +0300

    indents removed

--- src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/subscribe_invoke_tx_tests/SubscribeInvokeReissueTest.java
+++ src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/subscribe_invoke_tx_tests/SubscribeInvokeReissueTest.java
@@ -19,7 +19,6 @@ import static im.mak.paddle.util.Constants.WAVES_STRING_ID;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class SubscribeInvokeReissueTest extends InvokeBaseTest {
-
     @Test
     @DisplayName("subscribe invoke with Reissue")
     void subscribeInvokeWithReissue() {
@@ -40,7 +39,6 @@ public class SubscribeInvokeReissueTest extends InvokeBaseTest {
     }
 
     private void assertionsCheck() {
-
         assertAll(
                 () -> checkInvokeSubscribeTransaction(getFee(), getCallerPublicKey()),
                 () -> checkMainMetadata(0),

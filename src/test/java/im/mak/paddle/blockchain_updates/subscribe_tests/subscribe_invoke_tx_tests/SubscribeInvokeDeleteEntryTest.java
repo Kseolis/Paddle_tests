552ffc78bb479e5282e73f60eaedb0e88430d2ef
vnikolaenko <vnikolaenko@web3tech.ru>
2022-09-01 15:54:35 +0300

    refactoring SubscribeInvokeDeleteEntryTest

--- src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/subscribe_invoke_tx_tests/SubscribeInvokeDeleteEntryTest.java
+++ src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/subscribe_invoke_tx_tests/SubscribeInvokeDeleteEntryTest.java
@@ -1,11 +1,17 @@
-/*
 package im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_tx_tests;
 
+import com.wavesplatform.transactions.common.Amount;
+import com.wavesplatform.transactions.common.AssetId;
+import im.mak.paddle.Account;
+import im.mak.paddle.dapp.DAppCall;
 import im.mak.paddle.helpers.PrepareInvokeTestsData;
+import im.mak.paddle.helpers.transaction_senders.invoke.InvokeCalculationsBalancesAfterTx;
 import im.mak.paddle.helpers.transaction_senders.invoke.InvokeScriptTransactionSender;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static com.wavesplatform.transactions.InvokeScriptTransaction.LATEST_VERSION;
 import static im.mak.paddle.Node.node;
@@ -17,42 +23,54 @@ import static im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_
 import static im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_tx_tests.invoke_transactions_checkers.InvokeTransactionAssertions.checkPaymentsSubscribe;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.SubscribeHandler.subscribeResponseHandler;
 import static im.mak.paddle.helpers.transaction_senders.BaseTransactionSender.setVersion;
-import static im.mak.paddle.helpers.transaction_senders.invoke.InvokeScriptTransactionSender.getInvokeScriptId;
 import static im.mak.paddle.util.Constants.*;
 import static im.mak.paddle.util.Constants.WAVES_STRING_ID;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class SubscribeInvokeDeleteEntryTest extends InvokeBaseTest {
-    private static PrepareInvokeTestsData testsData;
+    private static PrepareInvokeTestsData testData;
+    private InvokeCalculationsBalancesAfterTx calcBalances;
 
     @BeforeAll
     static void before() {
-        testsData = new PrepareInvokeTestsData();
+        testData = new PrepareInvokeTestsData();
     }
 
     @Test
     @DisplayName("subscribe invoke with DeleteEntry")
     void subscribeInvokeWithDeleteEntry() {
         String intValueAfter = String.valueOf(0);
-        testsData.prepareDataForDeleteEntryTests();
+        testData.prepareDataForDeleteEntryTests();
+        calcBalances = new InvokeCalculationsBalancesAfterTx(testData);
 
-        InvokeScriptTransactionSender txSender = new InvokeScriptTransactionSender
-                (testsData.getCallerAccount(), testsData.getDAppAccount(), testsData.getDAppCall(), testsData.getAmounts());
+        final AssetId assetId = testData.getAssetId();
+        final DAppCall dAppCall = testData.getDAppCall();
+        final Account caller = testData.getCallerAccount();
+        final Account dAppAccount = testData.getDAppAccount();
+        final List<Amount> amounts = testData.getAmounts();
+
+        InvokeScriptTransactionSender txSender =
+                new InvokeScriptTransactionSender(caller, dAppAccount, dAppCall, amounts);
 
         setVersion(LATEST_VERSION);
-        balancesAfterPaymentInvoke(testsData.getCallerAccount(), testsData.getDAppAccount(), testsData.getAmounts(), testsData.getAssetId());
+        calcBalances.balancesAfterPaymentInvoke(caller, dAppAccount, amounts, assetId);
         txSender.invokeSenderWithPayment();
 
-        height = node().getHeight();
-        subscribeResponseHandler(CHANNEL, testsData.getDAppAccount(), height, height, getInvokeScriptId());
-        prepareInvoke(testsData.getDAppAccount(), testsData);
+        final String txId = txSender.getInvokeScriptId();
 
-        assertionsCheck(testsData.getWavesAmount().value(), String.valueOf(testsData.getIntArg()), intValueAfter);
+        height = node().getHeight();
+        subscribeResponseHandler(CHANNEL, dAppAccount, height, height, txId);
+        prepareInvoke(dAppAccount, testData);
+
+        assertionsCheck(testData.getWavesAmount().value(),
+                String.valueOf(testData.getIntArg()),
+                intValueAfter,
+                txId);
     }
 
-    private void assertionsCheck(long payment, String intVal, String valAfter) {
+    private void assertionsCheck(long payment, String intVal, String valAfter, String txId) {
         assertAll(
-                () -> checkInvokeSubscribeTransaction(testsData.getInvokeFee(), testsData.getCallerPublicKey()),
+                () -> checkInvokeSubscribeTransaction(testData.getInvokeFee(), testData.getCallerPublicKey(), txId),
                 () -> checkPaymentsSubscribe(0, 0, payment, ""),
 
                 () -> checkMainMetadata(0),
@@ -62,19 +80,19 @@ public class SubscribeInvokeDeleteEntryTest extends InvokeBaseTest {
 
                 () -> checkStateUpdateBalance(0,
                         0,
-                        testsData.getCallerAddress(),
+                        testData.getCallerAddress(),
                         WAVES_STRING_ID,
-                        getCallerBalanceWavesBeforeTransaction(), getCallerBalanceWavesAfterTransaction()),
+                        calcBalances.getCallerBalanceWavesBeforeTransaction(),
+                        calcBalances.getCallerBalanceWavesAfterTransaction()),
 
                 () -> checkStateUpdateBalance(0,
                         1,
                         getDAppAccountAddress(),
                         "",
-                        getDAppBalanceWavesBeforeTransaction(), getDAppBalanceWavesAfterTransaction()),
-
+                        calcBalances.getDAppBalanceWavesBeforeTransaction(),
+                        calcBalances.getDAppBalanceWavesAfterTransaction()),
 
                 () -> checkStateUpdateDataEntries(0, 0, getDAppAccountAddress(), DATA_ENTRY_INT, valAfter)
         );
     }
 }
-*/

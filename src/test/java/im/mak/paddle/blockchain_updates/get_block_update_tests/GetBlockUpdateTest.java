9d1ee31ea75d61e6854d63af2708a7cf39e730f1
vnikolaenko <vnikolaenko@web3tech.ru>
2022-09-19 16:00:24 +0300

    added aliasTx GetBlockUpdate test

--- src/test/java/im/mak/paddle/blockchain_updates/get_block_update_tests/GetBlockUpdateTest.java
+++ src/test/java/im/mak/paddle/blockchain_updates/get_block_update_tests/GetBlockUpdateTest.java
@@ -1,12 +1,22 @@
 package im.mak.paddle.blockchain_updates.get_block_update_tests;
 
-import im.mak.paddle.helpers.blockchain_updates_handlers.get_block_handlers.GetBlockUpdateHandler;
+import im.mak.paddle.blockchain_updates.GrpcTransactionsCheckers;
+import im.mak.paddle.helpers.blockchain_updates_handlers.GetBlockUpdateHandler;
 import org.junit.jupiter.api.Test;
+
+import static im.mak.paddle.util.Constants.MIN_FEE;
 
 public class GetBlockUpdateTest extends BaseGetBlockUpdateTest {
     @Test
-    void getBlockUpdateBaseTest() {
+    void getBlockUpdateAliasTransactionTest() {
         GetBlockUpdateHandler getBlockUpdateHandler = new GetBlockUpdateHandler();
-        getBlockUpdateHandler.getBlockUpdateResponseHandler(CHANNEL, sender, height, aliasTxId.toString());
+        getBlockUpdateHandler.getBlockUpdateResponseHandler(CHANNEL, heightsList, aliasTxId.toString());
+        GrpcTransactionsCheckers grpcTransactionsCheckers = new GrpcTransactionsCheckers(
+                getBlockUpdateHandler.getTxIndex(),
+                senderAddress.toString(),
+                senderPublicKey.toString(),
+                aliasTxId.toString()
+        );
+        grpcTransactionsCheckers.checkAliasGrpc(newAlias, amountBeforeAliasTx, amountAfterAliasTx, MIN_FEE);
     }
 }

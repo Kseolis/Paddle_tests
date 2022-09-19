8b36862fa44606d93e9214440788e67b27feb8fc
vnikolaenko <vnikolaenko@web3tech.ru>
2022-09-19 09:23:02 +0300

    refactoring transaction search

--- src/main/java/im/mak/paddle/helpers/blockchain_updates_handlers/get_block_handlers/GetBlockUpdateHandler.java
+++ src/main/java/im/mak/paddle/helpers/blockchain_updates_handlers/get_block_handlers/GetBlockUpdateHandler.java
@@ -3,68 +3,56 @@ package im.mak.paddle.helpers.blockchain_updates_handlers.get_block_handlers;
 import com.wavesplatform.crypto.base.Base58;
 import com.wavesplatform.events.api.grpc.protobuf.BlockchainUpdates.GetBlockUpdateRequest;
 import com.wavesplatform.events.api.grpc.protobuf.BlockchainUpdates.GetBlockUpdateResponse;
-import com.wavesplatform.events.api.grpc.protobuf.BlockchainUpdatesApiGrpc;
+import com.wavesplatform.events.api.grpc.protobuf.BlockchainUpdatesApiGrpc.BlockchainUpdatesApiBlockingStub;
 import com.wavesplatform.events.protobuf.Events.BlockchainUpdated.Append;
-import com.wavesplatform.protobuf.block.BlockOuterClass.MicroBlock;
-import com.wavesplatform.protobuf.transaction.TransactionOuterClass;
-import im.mak.paddle.Account;
 import io.grpc.Channel;
+
+import java.util.List;
 
 import static com.wavesplatform.events.api.grpc.protobuf.BlockchainUpdatesApiGrpc.newBlockingStub;
 
 public class GetBlockUpdateHandler {
     private Append append;
-    private MicroBlock microBlockInfo;
-    private TransactionOuterClass.Transaction firstTransaction;
     private String transactionId;
+    private int txIndex;
 
-    public void getBlockUpdateResponseHandler(Channel channel, Account account, int height, String txId) {
-        GetBlockUpdateRequest request = GetBlockUpdateRequest
-                .newBuilder()
-                .setHeight(height)
-                .build();
-
-        BlockchainUpdatesApiGrpc.BlockchainUpdatesApiBlockingStub stub = newBlockingStub(channel);
-        GetBlockUpdateResponse response = stub.getBlockUpdate(request);
-        blockUpdateEventHandler(response, account, txId);
+    public void getBlockUpdateResponseHandler(Channel channel, List<Integer> heights, String txId) {
+        for (int height : heights) {
+            if (append == null) {
+                GetBlockUpdateRequest request = GetBlockUpdateRequest
+                        .newBuilder()
+                        .setHeight(height)
+                        .build();
+                BlockchainUpdatesApiBlockingStub stub = newBlockingStub(channel);
+                GetBlockUpdateResponse response = stub.getBlockUpdate(request);
+                append = searchingForTransactionInBlock(response, txId);
+            }
+        }
     }
 
     public Append getAppend() {
         return append;
     }
 
-    public MicroBlock getMicroBlockInfo() {
-        return microBlockInfo;
-    }
-
-    public TransactionOuterClass.Transaction getFirstTransaction() {
-        return firstTransaction;
-    }
-
     public String getTransactionId() {
         return transactionId;
     }
 
-    private void blockUpdateEventHandler(GetBlockUpdateResponse response, Account account, String txId) {
-        final String accPublicKey = account.publicKey().toString();
-        append = response.getUpdate().getAppend();
-        microBlockInfo = append
-                .getMicroBlock()
-                .getMicroBlock()
-                .getMicroBlock();
-        System.out.println(append);
-        if (microBlockInfo.getTransactionsCount() > 0) {
+    private Append searchingForTransactionInBlock(GetBlockUpdateResponse response, String txId) {
+        Append tempAppend = response.getUpdate().getAppend();
+        long txIdsCount = tempAppend.getTransactionIdsCount() - 1;
 
-            String transactionSenderPublicKey = Base58.encode(microBlockInfo
-                    .getTransactions(0)
-                    .getWavesTransaction()
-                    .getSenderPublicKey()
-                    .toByteArray());
-
-            transactionId = Base58.encode(append.getTransactionIds(0).toByteArray());
-            if (transactionSenderPublicKey.equalsIgnoreCase(accPublicKey) && transactionId.equals(txId)) {
-                firstTransaction = microBlockInfo.getTransactions(0).getWavesTransaction();
+        for (int i = 0; i <= txIdsCount; i++) {
+            transactionId = Base58.encode(tempAppend.getTransactionIds(i).toByteArray());
+            if (transactionId.equals(txId)) {
+                txIndex = i;
+                return tempAppend;
             }
         }
+        return null;
+    }
+
+    public int getTxIndex() {
+        return txIndex;
     }
 }

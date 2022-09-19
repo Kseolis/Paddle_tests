72b1caa1d84b48d919cb15e31870a726444d36d1
vnikolaenko <vnikolaenko@web3tech.ru>
2022-09-19 17:13:33 +0300

    added GrpcIssueCheckers.java; added getTxVersion and getTxFeeAmount for TransactionsHandler; deleted firstTransaction variable; added issueTxId, amountBeforeIssueTx, amountAfterIssueTx variables; added getBlockUpdateIssueTransactionTest; edited checks invoke transactions and edited IssueTransactionSubscriptionGrpcTest;

--- src/test/java/im/mak/paddle/blockchain_updates/get_block_update_tests/GetBlockUpdateTest.java
+++ src/test/java/im/mak/paddle/blockchain_updates/get_block_update_tests/GetBlockUpdateTest.java
@@ -1,6 +1,7 @@
 package im.mak.paddle.blockchain_updates.get_block_update_tests;
 
 import im.mak.paddle.blockchain_updates.transactions_checkers.GrpcAliasCheckers;
+import im.mak.paddle.blockchain_updates.transactions_checkers.GrpcIssueCheckers;
 import im.mak.paddle.helpers.blockchain_updates_handlers.GetBlockUpdateHandler;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -20,5 +21,19 @@ public class GetBlockUpdateTest extends BaseGetBlockUpdateTest {
                 aliasTxId.toString()
         );
         grpcAliasCheckers.checkAliasGrpc(newAlias, amountBeforeAliasTx, amountAfterAliasTx, MIN_FEE);
+    }
+
+    @Test
+    @DisplayName("Check getBlockUpdate response for issue transaction")
+    void getBlockUpdateIssueTransactionTest() {
+        GetBlockUpdateHandler getBlockUpdateHandler = new GetBlockUpdateHandler();
+        getBlockUpdateHandler.getBlockUpdateResponseHandler(CHANNEL, heightsList, issueTxId.toString());
+        GrpcIssueCheckers getIssueCheckers = new GrpcIssueCheckers(
+                getBlockUpdateHandler.getTxIndex(),
+                senderAddress.toString(),
+                senderPublicKey.toString(),
+                issueTx
+        );
+        getIssueCheckers.checkIssueGrpc(amountBeforeIssueTx, amountAfterIssueTx);
     }
 }
--- src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/IssueTransactionSubscriptionGrpcTest.java
+++ src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/IssueTransactionSubscriptionGrpcTest.java
@@ -4,6 +4,7 @@ import com.wavesplatform.transactions.IssueTransaction;
 import com.wavesplatform.wavesj.info.IssueTransactionInfo;
 import im.mak.paddle.Account;
 import im.mak.paddle.blockchain_updates.BaseGrpcTest;
+import im.mak.paddle.blockchain_updates.transactions_checkers.GrpcIssueCheckers;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,17 +12,10 @@ import org.junit.jupiter.api.Test;
 import static im.mak.paddle.Node.node;
 import static im.mak.paddle.helpers.Randomizer.getRandomInt;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.SubscribeHandler.*;
-import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transaction_state_updates.Assets.*;
-import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transaction_state_updates.Balances.*;
-import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transactions_handlers.IssueTransactionHandler.*;
-import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transactions_handlers.TransactionsHandler.getChainId;
-import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transactions_handlers.TransactionsHandler.getSenderPublicKeyFromTransaction;
 import static im.mak.paddle.helpers.transaction_senders.BaseTransactionSender.setTxInfo;
 import static im.mak.paddle.util.Async.async;
 import static im.mak.paddle.util.Constants.*;
 import static im.mak.paddle.util.Constants.DEFAULT_FAUCET;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class IssueTransactionSubscriptionGrpcTest extends BaseGrpcTest {
     private static int assetDecimals;
@@ -30,13 +24,10 @@ public class IssueTransactionSubscriptionGrpcTest extends BaseGrpcTest {
     private static Account account;
     private static String assetName;
     private static String assetDescription;
-    private long fee = ONE_WAVES;
-    private byte[] compileScript = node().compileScript(SCRIPT_PERMITTING_OPERATIONS).script().bytes();
 
     private IssueTransactionInfo txInfo;
     private IssueTransaction tx;
     private String txId;
-    private String assetId;
 
     private long amountBefore;
     private long amountAfter;
@@ -72,14 +63,15 @@ public class IssueTransactionSubscriptionGrpcTest extends BaseGrpcTest {
                 .script(SCRIPT_PERMITTING_OPERATIONS));
         tx = txInfo.tx();
         txId = tx.id().toString();
-        assetId = tx.assetId().toString();
 
         setTxInfo(txInfo);
 
         height = node().getHeight();
 
         subscribeResponseHandler(CHANNEL, height, height, txId);
-        checkIssueTransactionSubscribe(assetQuantity, reissue);
+
+        GrpcIssueCheckers getIssueCheckers = new GrpcIssueCheckers(0, address, publicKey, tx);
+        getIssueCheckers.checkIssueGrpc(amountBefore, amountAfter);
     }
 
     @Test
@@ -87,7 +79,6 @@ public class IssueTransactionSubscriptionGrpcTest extends BaseGrpcTest {
     void subscribeTestForIssueTransaction() {
         amountBefore = account.getWavesBalance();
         amountAfter = amountBefore - ONE_WAVES;
-        compileScript = new byte[0];
         final int assetQuantity = getRandomInt(1000, 999_999_999);
         final boolean reissue = false;
 
@@ -99,69 +90,33 @@ public class IssueTransactionSubscriptionGrpcTest extends BaseGrpcTest {
                 .reissuable(reissue));
         tx = txInfo.tx();
         txId = tx.id().toString();
-        assetId = tx.assetId().toString();
 
         setTxInfo(txInfo);
 
         height = node().getHeight();
 
         subscribeResponseHandler(CHANNEL, height, height, txId);
-        checkIssueTransactionSubscribe(assetQuantity, reissue);
+        GrpcIssueCheckers getIssueCheckers = new GrpcIssueCheckers(0, address, publicKey, tx);
+        getIssueCheckers.checkIssueGrpc(amountBefore, amountAfter);
     }
 
     @Test
     @DisplayName("Check subscription on issue transaction")
     void subscribeTestForIssueNftTransaction() {
         amountBefore = account.getWavesBalance();
-        compileScript = new byte[0];
-        fee = MIN_FEE;
-        amountAfter = DEFAULT_FAUCET - fee;
+        amountAfter = DEFAULT_FAUCET - MIN_FEE;
         assetDecimals = 0;
 
         txInfo = account.issueNft(i -> i.name(assetName).description(assetDescription));
         tx = txInfo.tx();
         txId = tx.id().toString();
-        assetId = tx.assetId().toString();
 
         setTxInfo(txInfo);
 
         height = node().getHeight();
 
         subscribeResponseHandler(CHANNEL, height, height, txId);
-        checkIssueTransactionSubscribe(1, false);
-    }
-
-    private void checkIssueTransactionSubscribe(int quantity, boolean reissue) {
-        assertAll(
-                () -> assertThat(getChainId(0)).isEqualTo(CHAIN_ID),
-                () -> assertThat(getSenderPublicKeyFromTransaction(0)).isEqualTo(publicKey),
-                () -> assertThat(getAssetName(0)).isEqualTo(assetName),
-                () -> assertThat(getAssetDescription(0)).isEqualTo(assetDescription),
-                () -> assertThat(getAssetAmount(0)).isEqualTo(quantity),
-                () -> assertThat(getAssetReissuable(0)).isEqualTo(reissue),
-                () -> assertThat(getAssetDecimals(0)).isEqualTo(assetDecimals),
-                () -> assertThat(getAssetScript(0)).isEqualTo(compileScript),
-                () -> assertThat(getFirstTransaction().getVersion()).isEqualTo(IssueTransaction.LATEST_VERSION),
-                () -> assertThat(getFirstTransaction().getFee().getAmount()).isEqualTo(fee),
-                () -> assertThat(getTransactionId()).isEqualTo(tx.id().toString()),
-                () -> assertThat(getAddress(0, 0)).isEqualTo(address),
-                // check waves balance from balances
-                () -> assertThat(getAmountBefore(0, 0)).isEqualTo(amountBefore),
-                () -> assertThat(getAmountAfter(0, 0)).isEqualTo(amountAfter),
-                // check assetId and balance from balances
-                () -> assertThat(getAssetIdAmountAfter(0, 1)).isEqualTo(assetId),
-                () -> assertThat(getAmountBefore(0, 1)).isEqualTo(0),
-                () -> assertThat(getAmountAfter(0, 1)).isEqualTo(quantity),
-                // check from assets
-                () -> assertThat(getAssetIdFromAssetAfter(0, 0)).isEqualTo(assetId),
-                () -> assertThat(getIssuerAfter(0, 0)).isEqualTo(publicKey),
-                // check asset info
-                () -> assertThat(getNameAfter(0, 0)).isEqualTo(assetName),
-                () -> assertThat(getDescriptionAfter(0, 0)).isEqualTo(assetDescription),
-                () -> assertThat(getQuantityAfter(0, 0)).isEqualTo(String.valueOf(quantity)),
-                () -> assertThat(getDecimalsAfter(0, 0)).isEqualTo(String.valueOf(assetDecimals)),
-                () -> assertThat(getReissueAfter(0, 0)).isEqualTo(String.valueOf(reissue)),
-                () -> assertThat(getScriptAfter(0, 0)).isEqualTo(compileScript)
-        );
+        GrpcIssueCheckers getIssueCheckers = new GrpcIssueCheckers(0, address, publicKey, tx);
+        getIssueCheckers.checkIssueGrpc(amountBefore, amountAfter);
     }
 }

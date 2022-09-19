ad532dee4beea060b26dd4f6835cbd9ba3bbe055
vnikolaenko <vnikolaenko@web3tech.ru>
2022-09-19 15:59:50 +0300

    added checkAliasGrpc checker method

--- src/test/java/im/mak/paddle/blockchain_updates/GrpcTransactionsCheckers.java
+++ src/test/java/im/mak/paddle/blockchain_updates/GrpcTransactionsCheckers.java
@@ -2,7 +2,6 @@ package im.mak.paddle.blockchain_updates;
 
 import static com.wavesplatform.transactions.CreateAliasTransaction.LATEST_VERSION;
 import static im.mak.paddle.blockchain_updates.BaseGrpcTest.CHAIN_ID;
-import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.SubscribeHandler.getTransactionId;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transaction_state_updates.Balances.*;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transactions_handlers.AliasTransactionHandler.getAliasFromAliasTransaction;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transactions_handlers.TransactionsHandler.*;
@@ -11,19 +10,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class GrpcTransactionsCheckers {
+    private final int txIndex;
+    private final String address;
+    private final String publicKey;
+    private final String aliasTxId;
 
-    public static void checkAliasSubscribe
-            (String newAlias, long amountBefore, long amountAfter, String address, String publicKey, long fee) {
+    public GrpcTransactionsCheckers(int txIndex, String address, String publicKey, String aliasTxId) {
+        this.txIndex = txIndex;
+        this.address = address;
+        this.publicKey = publicKey;
+        this.aliasTxId = aliasTxId;
+    }
+
+    public void checkAliasGrpc(String newAlias, long amountBefore, long amountAfter, long fee) {
         assertAll(
-                () -> assertThat(getChainId(0)).isEqualTo(CHAIN_ID),
-                () -> assertThat(getSenderPublicKeyFromTransaction(0)).isEqualTo(publicKey),
-                () -> assertThat(getAliasFromAliasTransaction(0)).isEqualTo(newAlias),
-                () -> assertThat(getTransactionVersion(0)).isEqualTo(LATEST_VERSION),
-                () -> assertThat(getTransactionFeeAmount(0)).isEqualTo(fee),
-                () -> assertThat(getAddress(0, 0)).isEqualTo(address),
-                () -> assertThat(getAmountBefore(0, 0)).isEqualTo(amountBefore),
-                () -> assertThat(getAmountAfter(0, 0)).isEqualTo(amountAfter),
-                () -> assertThat(getTransactionId()).isEqualTo(getTransactionId())
+                () -> assertThat(getChainId(txIndex)).isEqualTo(CHAIN_ID),
+                () -> assertThat(getSenderPublicKeyFromTransaction(txIndex)).isEqualTo(publicKey),
+                () -> assertThat(getAliasFromAliasTransaction(txIndex)).isEqualTo(newAlias),
+                () -> assertThat(getTransactionVersion(txIndex)).isEqualTo(LATEST_VERSION),
+                () -> assertThat(getTransactionFeeAmount(txIndex)).isEqualTo(fee),
+                () -> assertThat(getAddress(txIndex, 0)).isEqualTo(address),
+                () -> assertThat(getAmountBefore(txIndex, 0)).isEqualTo(amountBefore),
+                () -> assertThat(getAmountAfter(txIndex, 0)).isEqualTo(amountAfter),
+                () -> assertThat(getTxId(txIndex)).isEqualTo(aliasTxId)
         );
     }
 }

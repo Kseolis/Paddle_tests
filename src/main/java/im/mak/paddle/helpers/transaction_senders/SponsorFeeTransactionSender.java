a3d69ef322579c079a7527d93d046c47df63e521
vnikolaenko <vnikolaenko@web3tech.ru>
2022-10-06 17:17:07 +0300

    added wavesAmountBefore, wavesAmountAfter refactoring SponsorFeeTransactionSubscriptionGrpcTest move checkSponsorFeeGrpc to GrpcSponsorFeeCheckers

--- src/main/java/im/mak/paddle/helpers/transaction_senders/SponsorFeeTransactionSender.java
+++ src/main/java/im/mak/paddle/helpers/transaction_senders/SponsorFeeTransactionSender.java
@@ -6,8 +6,7 @@ import im.mak.paddle.Account;
 import im.mak.paddle.exceptions.ApiError;
 
 import static im.mak.paddle.Node.node;
-import static im.mak.paddle.util.Constants.EXTRA_FEE;
-import static im.mak.paddle.util.Constants.SUM_FEE;
+import static im.mak.paddle.util.Constants.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SponsorFeeTransactionSender extends BaseTransactionSender {
@@ -15,6 +14,8 @@ public class SponsorFeeTransactionSender extends BaseTransactionSender {
     private final Account assetOwner;
     private final long sponsorFee;
     private final AssetId assetId;
+    private long wavesAmountBefore;
+    private long wavesAmountAfter;
 
     public SponsorFeeTransactionSender(Account assetOwner, long sponsorFee, AssetId assetId) {
         this.assetOwner = assetOwner;
@@ -23,6 +24,8 @@ public class SponsorFeeTransactionSender extends BaseTransactionSender {
     }
 
     public void sponsorFeeTransactionSender(long fee, int version) {
+        wavesAmountBefore = assetOwner.getWavesBalance();
+        wavesAmountAfter = wavesAmountBefore - fee;
         sponsorTx = SponsorFeeTransaction.builder(assetId, sponsorFee)
                 .version(version)
                 .fee(fee)
@@ -32,6 +35,8 @@ public class SponsorFeeTransactionSender extends BaseTransactionSender {
     }
 
     public void cancelSponsorFeeSender(Account assetOwner, Account sender, Account recipient, int version, long extraFee) {
+        wavesAmountBefore = assetOwner.getWavesBalance();
+        wavesAmountAfter = wavesAmountBefore - MIN_FEE - extraFee;
         sponsorTx = SponsorFeeTransaction.builder(assetId, 0)
                 .version(version)
                 .extraFee(extraFee)
@@ -54,12 +59,19 @@ public class SponsorFeeTransactionSender extends BaseTransactionSender {
     public long getSponsorFee() {
         return sponsorFee;
     }
-
     public AssetId getAssetId() {
         return assetId;
     }
 
     public SponsorFeeTransaction getSponsorTx() {
         return sponsorTx;
+    }
+
+    public long getWavesAmountBefore() {
+        return wavesAmountBefore;
+    }
+
+    public long getWavesAmountAfter() {
+        return wavesAmountAfter;
     }
 }

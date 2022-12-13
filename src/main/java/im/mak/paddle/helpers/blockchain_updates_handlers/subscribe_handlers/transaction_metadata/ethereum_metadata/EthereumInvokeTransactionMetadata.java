8df1ed7ab735ab2a2972c272d1e805d4eecfc721
vnikolaenko <vnikolaenko@web3tech.ru>
2022-12-13 17:59:16 +0300

    from EthereumInvokeTransactionMetadata.java transferred methods

--- src/main/java/im/mak/paddle/helpers/blockchain_updates_handlers/subscribe_handlers/transaction_metadata/ethereum_metadata/EthereumInvokeTransactionMetadata.java
+++ src/main/java/im/mak/paddle/helpers/blockchain_updates_handlers/subscribe_handlers/transaction_metadata/ethereum_metadata/EthereumInvokeTransactionMetadata.java
@@ -3,11 +3,17 @@ package im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.tra
 import com.wavesplatform.crypto.base.Base58;
 import com.wavesplatform.events.protobuf.Events.TransactionMetadata.InvokeScriptMetadata;
 import com.wavesplatform.protobuf.transaction.InvokeScriptResultOuterClass.InvokeScriptResult;
+import com.wavesplatform.protobuf.transaction.InvokeScriptResultOuterClass.InvokeScriptResult.Invocation;
 import com.wavesplatform.protobuf.transaction.InvokeScriptResultOuterClass.InvokeScriptResult.Issue;
 
+import static im.mak.paddle.helpers.blockchain_updates_handlers.AppendHandler.getAppend;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transaction_metadata.ethereum_metadata.EthereumTransactionMetadata.getEthereumTransactionMetadata;
 
 public class EthereumInvokeTransactionMetadata {
+    public static InvokeScriptMetadata getElementTransactionEthereumInvokeMetadata(int metadataIndex) {
+        return getAppend().getTransactionsMetadata(metadataIndex).getEthereum().getInvoke();
+    }
+
     public static String getEthereumInvokeDAppAddress(int metadataIndex) {
         return Base58.encode(getEthereumInvokeTransactionMetadata(metadataIndex).getDAppAddress().toByteArray());
     }
@@ -16,56 +22,16 @@ public class EthereumInvokeTransactionMetadata {
         return getEthereumInvokeTransactionMetadata(metadataIndex).getFunctionName();
     }
 
-    public static String getEthereumInvokeStringArgument(int metadataIndex, int argumentIndex) {
-        return getEthereumInvokeTransactionMetadata(metadataIndex).getArguments(argumentIndex).getStringValue();
-    }
-
-    public static String getEthereumInvokeBinaryArgument(int metadataIndex, int argumentIndex) {
-        return Base58.encode(getEthereumInvokeTransactionMetadata(metadataIndex).getArguments(argumentIndex).getBinaryValue().toByteArray());
-    }
-
-    public static boolean getEthereumInvokeBooleanArgument(int metadataIndex, int argumentIndex) {
-        return getEthereumInvokeTransactionMetadata(metadataIndex).getArguments(argumentIndex).getBooleanValue();
-    }
-
-    public static long getEthereumInvokeIntegerArgument(int metadataIndex, int argumentIndex) {
-        return getEthereumInvokeTransactionMetadata(metadataIndex).getArguments(argumentIndex).getIntegerValue();
-    }
-
-    public static String getEthereumInvokeIssuesAssetId(int metadataIndex, int issueIndex) {
-        return Base58.encode(getEthereumInvokeResultIssues(metadataIndex, issueIndex).getAssetId().toByteArray());
-    }
-
-    public static String getEthereumInvokeIssuesName(int metadataIndex, int issueIndex) {
-        return getEthereumInvokeResultIssues(metadataIndex, issueIndex).getName();
-    }
-
-    public static String getEthereumInvokeIssuesDescription(int metadataIndex, int issueIndex) {
-        return getEthereumInvokeResultIssues(metadataIndex, issueIndex).getDescription();
-    }
-
-    public static long getEthereumInvokeIssuesAmount(int metadataIndex, int issueIndex) {
-        return getEthereumInvokeResultIssues(metadataIndex, issueIndex).getAmount();
-    }
-
-    public static long getEthereumInvokeIssuesDecimals(int metadataIndex, int issueIndex) {
-        return getEthereumInvokeResultIssues(metadataIndex, issueIndex).getDecimals();
-    }
-
-    public static boolean getEthereumInvokeIssuesReissuable(int metadataIndex, int issueIndex) {
-        return getEthereumInvokeResultIssues(metadataIndex, issueIndex).getReissuable();
-    }
-
-    public static long getEthereumInvokeIssuesNonce(int metadataIndex, int issueIndex) {
-        return getEthereumInvokeResultIssues(metadataIndex, issueIndex).getNonce();
-    }
-
     static InvokeScriptMetadata getEthereumInvokeTransactionMetadata(int metadataIndex) {
         return getEthereumTransactionMetadata(metadataIndex).getInvoke();
     }
 
     static InvokeScriptResult getEthereumInvokeResult(int metadataIndex) {
         return getEthereumInvokeTransactionMetadata(metadataIndex).getResult();
+    }
+
+    static Invocation getEthereumInvokeResultInvokes(int metadataIndex, int invokeIndex) {
+        return getEthereumInvokeResult(metadataIndex).getInvokes(invokeIndex);
     }
 
     static Issue getEthereumInvokeResultIssues(int metadataIndex, int issueIndex) {

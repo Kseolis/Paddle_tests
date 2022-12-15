5a74832e78c667b1b6a0b860d8888bd9ed264504
vnikolaenko <vnikolaenko@web3tech.ru>
2022-12-15 15:52:05 +0300

    refactoring test SubscribeInvokeLeaseGrpcTest

--- src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/subscribe_invoke_tx_tests/SubscribeInvokeLeaseGrpcTest.java
+++ src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/subscribe_invoke_tx_tests/SubscribeInvokeLeaseGrpcTest.java
@@ -1,14 +1,13 @@
 package im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_invoke_tx_tests;
 
 import com.wavesplatform.transactions.common.Amount;
-import com.wavesplatform.transactions.common.AssetId;
 import im.mak.paddle.Account;
 import im.mak.paddle.blockchain_updates.BaseGrpcTest;
 import im.mak.paddle.dapp.DAppCall;
 import im.mak.paddle.helpers.PrepareInvokeTestsData;
 import im.mak.paddle.helpers.transaction_senders.invoke.InvokeCalculationsBalancesAfterTx;
 import im.mak.paddle.helpers.transaction_senders.invoke.InvokeScriptTransactionSender;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -19,81 +18,97 @@ import static im.mak.paddle.Node.node;
 import static im.mak.paddle.blockchain_updates.transactions_checkers.invoke_transactions_checkers.InvokeMetadataAssertions.*;
 import static im.mak.paddle.blockchain_updates.transactions_checkers.invoke_transactions_checkers.InvokeStateUpdateAssertions.*;
 import static im.mak.paddle.blockchain_updates.transactions_checkers.invoke_transactions_checkers.InvokeTransactionAssertions.*;
+import static im.mak.paddle.helpers.blockchain_updates_handlers.SubscribeHandler.getTxIndex;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.SubscribeHandler.subscribeResponseHandler;
 import static im.mak.paddle.helpers.transaction_senders.BaseTransactionSender.setVersion;
+import static im.mak.paddle.util.Async.async;
 import static im.mak.paddle.util.Constants.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class SubscribeInvokeLeaseGrpcTest extends BaseGrpcTest {
-    private static PrepareInvokeTestsData testData;
+    private PrepareInvokeTestsData testData;
     private InvokeCalculationsBalancesAfterTx calcBalances;
+    private DAppCall dAppCall;
+    private Account caller;
+    private String callerAddress;
+    private String callerPK;
+    private long callerBalanceWavesBeforeTx;
+    private long callerBalanceWavesAfterTx;
+    private Account dAppAccount;
+    private String dAppAddress;
+    private String dAppPK;
+    private long dAppBalanceWavesBeforeTx;
+    private long dAppBalanceWavesAfterTx;
+    private long invokeFee;
+    private List<Amount> payments;
+    private long amountValue;
 
-    @BeforeAll
-    static void before() {
+    @BeforeEach
+    void before() {
         testData = new PrepareInvokeTestsData();
+        testData.prepareDataForLeaseTests(SUM_FEE, ONE_WAVES);
+        setVersion(LATEST_VERSION);
+
+        async(
+                () -> {
+                    dAppCall = testData.getDAppCall();
+                    invokeFee = testData.getInvokeFee();
+                },
+                () -> {
+                    caller = testData.getCallerAccount();
+                    callerAddress = testData.getCallerAddress();
+                    callerPK = testData.getCallerPublicKey();
+                },
+                () -> {
+                    dAppAccount = testData.getDAppAccount();
+                    dAppAddress = testData.getDAppAddress();
+                    dAppPK = testData.getDAppPublicKey();
+                },
+                () -> payments = testData.getPayments(),
+                () -> amountValue = testData.getWavesAmount().value()
+        );
+
+        calcBalances = new InvokeCalculationsBalancesAfterTx(testData);
+        calcBalances.balancesAfterPaymentInvoke(caller.address(), dAppAccount.address(), payments, testData.getAssetId());
+
+        async(
+                () -> {
+                    callerBalanceWavesBeforeTx = calcBalances.getCallerBalanceWavesBeforeTransaction();
+                    callerBalanceWavesAfterTx = calcBalances.getCallerBalanceWavesAfterTransaction();
+                },
+                () -> {
+                    dAppBalanceWavesBeforeTx = calcBalances.getDAppBalanceWavesBeforeTransaction();
+                    dAppBalanceWavesAfterTx = calcBalances.getDAppBalanceWavesAfterTransaction();
+                }
+        );
     }
 
     @Test
     @DisplayName("subscribe invoke with Lease and WAVES payment")
     void subscribeInvokeWithLease() {
-        long amountValue = testData.getWavesAmount().value();
-        calcBalances = new InvokeCalculationsBalancesAfterTx(testData);
-        testData.prepareDataForLeaseTests(SUM_FEE, ONE_WAVES);
-
-        final AssetId assetId = testData.getAssetId();
-        final DAppCall dAppCall = testData.getDAppCall();
-        final Account caller = testData.getCallerAccount();
-        final Account dAppAccount = testData.getDAppAccount();
-        final List<Amount> amounts = testData.getPayments();
-
-        InvokeScriptTransactionSender txSender = new InvokeScriptTransactionSender(caller, dAppAccount, dAppCall, amounts);
-
-        setVersion(LATEST_VERSION);
-        calcBalances.balancesAfterPaymentInvoke(caller.address(), dAppAccount.address(), amounts, assetId);
+        InvokeScriptTransactionSender txSender = new InvokeScriptTransactionSender(caller, dAppAccount, dAppCall, payments);
         txSender.invokeSenderWithPayment();
-
         String txId = txSender.getInvokeScriptId();
-
         height = node().getHeight();
         subscribeResponseHandler(CHANNEL, height, height, txId);
         prepareInvoke(dAppAccount, testData);
-
-        assertionsCheck(amountValue, txId);
+        assertionsCheck(getTxIndex(), txId);
     }
 
-    private void assertionsCheck(long amountValue, String txId) {
+    private void assertionsCheck(int txIndex, String txId) {
         assertAll(
-                () -> checkInvokeSubscribeTransaction(testData.getInvokeFee(), testData.getCallerPublicKey(), txId, 0),
-                () -> checkMainMetadata(0),
-                () -> checkPaymentsSubscribe(0, 0, amountValue, ""),
-                () -> checkPaymentMetadata(0, 0, null, amountValue),
-                () -> checkLeaseMetadata(0, 0, testData.getCallerPublicKeyHash(), amountValue),
-
-                () -> checkStateUpdateBalance(0,
-                        0,
-                        testData.getCallerAddress(),
-                        WAVES_STRING_ID,
-                        calcBalances.getCallerBalanceWavesBeforeTransaction(),
-                        calcBalances.getCallerBalanceWavesAfterTransaction()),
-
-                () -> checkStateUpdateBalance(0,
-                        1,
-                        testData.getDAppAddress(),
-                        null,
-                        calcBalances.getDAppBalanceWavesBeforeTransaction(),
-                        calcBalances.getDAppBalanceWavesAfterTransaction()),
-
-                () -> checkStateUpdateBeforeLeasing(0, 0, testData.getCallerAddress(), 0, 0),
-                () -> checkStateUpdateBeforeLeasing(0, 1, testData.getDAppAddress(), 0, 0),
-
-                () -> checkStateUpdateAfterLeasing(0, 0, testData.getCallerAddress(), amountValue, 0),
-                () -> checkStateUpdateAfterLeasing(0, 1, testData.getDAppAddress(), 0, amountValue),
-
-                () -> checkStateUpdateIndividualLeases(0, 0,
-                        amountValue,
-                        testData.getDAppPublicKey(),
-                        testData.getCallerAddress(),
-                        ACTIVE_STATUS_LEASE)
+                () -> checkInvokeSubscribeTransaction(invokeFee, callerPK, txId, txIndex),
+                () -> checkMainMetadata(txIndex),
+                () -> checkPaymentsSubscribe(txIndex, 0, amountValue, WAVES_STRING_ID),
+                () -> checkPaymentMetadata(txIndex, 0, null, amountValue),
+                () -> checkLeaseMetadata(txIndex, 0, testData.getCallerPublicKeyHash(), amountValue),
+                () -> checkStateUpdateBalance(txIndex, 0, callerAddress, WAVES_STRING_ID, callerBalanceWavesBeforeTx, callerBalanceWavesAfterTx),
+                () -> checkStateUpdateBalance(txIndex, 1, dAppAddress, WAVES_STRING_ID, dAppBalanceWavesBeforeTx, dAppBalanceWavesAfterTx),
+                () -> checkStateUpdateBeforeLeasing(txIndex, 0, callerAddress, 0, 0),
+                () -> checkStateUpdateBeforeLeasing(txIndex, 1, dAppAddress, 0, 0),
+                () -> checkStateUpdateAfterLeasing(txIndex, 0, callerAddress, amountValue, 0),
+                () -> checkStateUpdateAfterLeasing(txIndex, 1, dAppAddress, 0, amountValue),
+                () -> checkStateUpdateIndividualLeases(txIndex, 0, amountValue, dAppPK, callerAddress, ACTIVE_STATUS_LEASE)
         );
     }
 }

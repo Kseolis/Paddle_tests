2adf39471858bd4461b65aac40e50488e003cdc6
vnikolaenko <vnikolaenko@web3tech.ru>
2022-12-19 15:52:25 +0300

    added checkers for SubscribeEthereumInvokeScriptTransferGrpcTest test correctly worked

--- src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/subscribe_ethereum_invoke_tx_tests/SubscribeEthereumInvokeScriptTransferGrpcTest.java
+++ src/test/java/im/mak/paddle/blockchain_updates/subscribe_tests/subscribe_ethereum_invoke_tx_tests/SubscribeEthereumInvokeScriptTransferGrpcTest.java
@@ -1,144 +1,173 @@
 package im.mak.paddle.blockchain_updates.subscribe_tests.subscribe_ethereum_invoke_tx_tests;
 
+import com.wavesplatform.transactions.account.Address;
 import com.wavesplatform.transactions.common.Amount;
 import com.wavesplatform.transactions.common.AssetId;
+import com.wavesplatform.transactions.invocation.Function;
+import com.wavesplatform.wavesj.exceptions.NodeException;
 import im.mak.paddle.Account;
 import im.mak.paddle.blockchain_updates.BaseGrpcTest;
 import im.mak.paddle.dapp.DAppCall;
+import im.mak.paddle.helpers.EthereumTestUser;
 import im.mak.paddle.helpers.PrepareInvokeTestsData;
+import im.mak.paddle.helpers.transaction_senders.EthereumInvokeTransactionSender;
 import im.mak.paddle.helpers.transaction_senders.invoke.InvokeCalculationsBalancesAfterTx;
-import im.mak.paddle.helpers.transaction_senders.invoke.InvokeScriptTransactionSender;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.List;
 
 import static com.wavesplatform.transactions.InvokeScriptTransaction.LATEST_VERSION;
 import static im.mak.paddle.Node.node;
-import static im.mak.paddle.blockchain_updates.transactions_checkers.invoke_transactions_checkers.InvokeMetadataAssertions.*;
+import static im.mak.paddle.blockchain_updates.transactions_checkers.ethereum_invoke_transaction_checkers.EthereumInvokeMetadataAssertions.*;
 import static im.mak.paddle.blockchain_updates.transactions_checkers.invoke_transactions_checkers.InvokeStateUpdateAssertions.checkStateUpdateAssets;
 import static im.mak.paddle.blockchain_updates.transactions_checkers.invoke_transactions_checkers.InvokeStateUpdateAssertions.checkStateUpdateBalance;
-import static im.mak.paddle.blockchain_updates.transactions_checkers.invoke_transactions_checkers.InvokeTransactionAssertions.checkInvokeSubscribeTransaction;
 import static im.mak.paddle.helpers.ConstructorRideFunctions.getIssueAssetData;
 import static im.mak.paddle.helpers.ConstructorRideFunctions.getIssueAssetVolume;
+import static im.mak.paddle.helpers.EthereumTestUser.getEthInstance;
+import static im.mak.paddle.helpers.blockchain_updates_handlers.SubscribeHandler.getTxIndex;
 import static im.mak.paddle.helpers.blockchain_updates_handlers.SubscribeHandler.subscribeResponseHandler;
+import static im.mak.paddle.helpers.blockchain_updates_handlers.subscribe_handlers.transactions_handlers.waves_transactions_handlers.WavesTransactionsHandler.getTxId;
 import static im.mak.paddle.helpers.transaction_senders.BaseTransactionSender.setVersion;
+import static im.mak.paddle.util.Async.async;
 import static im.mak.paddle.util.Constants.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class SubscribeEthereumInvokeScriptTransferGrpcTest extends BaseGrpcTest {
-    private static PrepareInvokeTestsData testData;
+    private PrepareInvokeTestsData testData;
     private InvokeCalculationsBalancesAfterTx calcBalances;
-    private String assetIdToStr;
+    private EthereumTestUser ethInstance;
+    private Address senderAddress;
+    private String senderAddressStr;
+    private long senderBalanceWavesBeforeTx;
+    private long senderBalanceWavesAfterTx;
+    private long senderAssetBalanceBeforeTx;
+    private long senderAssetBalanceAfterTx;
+    private DAppCall dAppCall;
+    private Function dAppCallFunction;
+    private Account assetDAppAccount;
+    private Address assetDAppAddress;
+    private String assetDAppAddressStr;
+    private long wavesAccDAppBalanceBeforeTx;
+    private long wavesAccDAppBalanceAfterTx;
+    private Account dAppAccount;
+    private Address dAppAddress;
+    private String dAppAddressStr;
+    private long wavesDAppBalanceBeforeTx;
+    private long wavesDAppBalanceAfterTx;
+    private long dAppAssetBalanceBeforeTx;
+    private long dAppAssetBalanceAfterTx;
+    private AssetId assetId;
+    private String assetIdStr;
+    private long invokeFee;
+    private List<Amount> payments;
+    private long assetPayment;
+    private long wavesPayment;
+    private long dAppAssetAmountAfter;
 
-    @BeforeAll
-    static void before() {
+    @BeforeEach
+    void before() {
         testData = new PrepareInvokeTestsData();
+        testData.prepareDataForScriptTransferTests();
+        setVersion(LATEST_VERSION);
+        async(
+                () -> {
+                    dAppCall = testData.getDAppCall();
+                    dAppCallFunction = dAppCall.getFunction();
+                    invokeFee = testData.getInvokeFee();
+                },
+                () -> {
+                    try {
+                        ethInstance = getEthInstance();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    senderAddress = ethInstance.getSenderAddress();
+                    senderAddressStr = senderAddress.toString();
+                    node().faucet().transfer(senderAddress, DEFAULT_FAUCET, AssetId.WAVES, i -> i.additionalFee(0));
+                },
+                () -> {
+                    dAppAccount = testData.getDAppAccount();
+                    dAppAddress = dAppAccount.address();
+                    dAppAddressStr = testData.getDAppAddress();
+                },
+                () -> {
+                    assetDAppAccount = testData.getAssetDAppAccount();
+                    assetDAppAddress = assetDAppAccount.address();
+                    assetDAppAddressStr = testData.getAssetDAppAddress();
+                },
+                () -> {
+                    assetId = testData.getAssetId();
+                    assetIdStr = assetId.toString();
+                },
+                () -> {
+                    payments = testData.getOtherAmounts();
+                    assetPayment = testData.getAssetAmount().value();
+                    wavesPayment = testData.getWavesAmount().value();
+                    dAppAssetAmountAfter = Long.parseLong(getIssueAssetData().get(VOLUME)) - assetPayment;
+                }
+        );
+        assetDAppAccount.transfer(senderAddress, Amount.of(assetPayment, testData.getAssetId()));
+        calcBalances = new InvokeCalculationsBalancesAfterTx(testData);
+        calcBalances.balancesAfterEthereumCallerScriptTransfer(senderAddress, assetDAppAddress, dAppAddress, payments, assetId);
+        async(
+                () -> senderBalanceWavesBeforeTx = calcBalances.getCallerBalanceWavesBeforeTransaction(),
+                () -> senderBalanceWavesAfterTx = calcBalances.getCallerBalanceWavesAfterTransaction(),
+                () -> senderAssetBalanceBeforeTx = calcBalances.getCallerBalanceIssuedAssetsBeforeTransaction(),
+                () -> senderAssetBalanceAfterTx = calcBalances.getCallerBalanceIssuedAssetsAfterTransaction(),
+
+                () -> wavesAccDAppBalanceBeforeTx = calcBalances.getDAppBalanceWavesBeforeTransaction(),
+                () -> wavesAccDAppBalanceAfterTx = calcBalances.getDAppBalanceWavesAfterTransaction(),
+
+                () -> wavesDAppBalanceBeforeTx = calcBalances.getAccBalanceWavesBeforeTransaction(),
+                () -> wavesDAppBalanceAfterTx = calcBalances.getAccBalanceWavesAfterTransaction(),
+                () -> dAppAssetBalanceBeforeTx = calcBalances.getAccBalanceIssuedAssetsBeforeTransaction(),
+                () -> dAppAssetBalanceAfterTx = calcBalances.getAccBalanceIssuedAssetsAfterTransaction()
+
+        );
     }
 
     @Test
-    @DisplayName("subscribe invoke with ScriptTransfer")
-    void subscribeInvokeWithScriptTransfer() {
-        long assetAmountValue = testData.getAssetAmount().value();
-        testData.prepareDataForScriptTransferTests();
-        calcBalances = new InvokeCalculationsBalancesAfterTx(testData);
-
-        final AssetId assetId = testData.getAssetId();
-        assetIdToStr = assetId.toString();
-
-        final DAppCall dAppCall = testData.getDAppCall();
-        final Account caller = testData.getCallerAccount();
-        final Account assetDAppAccount = testData.getAssetDAppAccount();
-        final Account dAppAccount = testData.getDAppAccount();
-        final List<Amount> amounts = testData.getPayments();
-
-        InvokeScriptTransactionSender txSender = new InvokeScriptTransactionSender(caller, assetDAppAccount, dAppCall);
-
-        setVersion(LATEST_VERSION);
-        calcBalances.balancesAfterCallerScriptTransfer(caller.address(), assetDAppAccount.address(), dAppAccount.address(), amounts, assetId);
-        txSender.invokeSender();
-
-        String txId = txSender.getInvokeScriptId();
-
+    @DisplayName("subscribe ethereum invoke with ScriptTransfer")
+    void subscribeInvokeWithScriptTransfer() throws NodeException, IOException {
+        EthereumInvokeTransactionSender txSender = new EthereumInvokeTransactionSender(assetDAppAddress, payments, invokeFee);
+        txSender.sendingAnEthereumInvokeTransaction(dAppCallFunction);
+        String txId = txSender.getEthTxId().toString();
         height = node().getHeight();
         subscribeResponseHandler(CHANNEL, height, height, txId);
         prepareInvoke(assetDAppAccount, testData);
-
-        long dAppAssetAmountAfter = Long.parseLong(getIssueAssetData().get(VOLUME)) - assetAmountValue;
-        assertionsCheck(dAppAssetAmountAfter, assetAmountValue, txId);
+        assertionsCheck(txSender, getTxIndex());
     }
 
-    private void assertionsCheck(long dAppAssetAmountAfter, long recipientAmountValueAfter, String txId) {
+    private void assertionsCheck(EthereumInvokeTransactionSender txSender, int txIndex) {
         assertAll(
-                () -> checkInvokeSubscribeTransaction(testData.getInvokeFee(), testData.getCallerPublicKey(), txId, 0),
+                () -> assertThat(getTxId(txIndex)).isEqualTo(txSender.getEthTx().id().toString()),
+                () -> checkEthereumMainMetadata(txSender, txIndex, senderAddressStr),
+                () -> checkEthereumInvokeMainInfo(txIndex, assetDAppAddressStr, dAppCallFunction),
 
-                () -> checkMainMetadata(0),
-                () -> checkArgumentsMetadata(0, 0, BINARY_BASE58, assetIdToStr),
-                () -> checkArgumentsMetadata(0, 1, BINARY_BASE58, testData.getDAppAddress()),
-                () -> checkIssueAssetMetadata(0, 0, getIssueAssetData()),
+                () -> checkArgumentsEthereumMetadata(txIndex, 0, BINARY_BASE58, assetIdStr),
+                () -> checkArgumentsEthereumMetadata(txIndex, 1, BINARY_BASE58, dAppAddressStr),
 
-                () -> checkTransfersMetadata(0, 0,
-                        testData.getDAppAddress(),
-                        assetIdToStr,
-                        testData.getAssetAmount().value()),
-                () -> checkTransfersMetadata(0, 1,
-                        testData.getDAppAddress(),
-                        null,
-                        testData.getAssetAmount().value()),
-                () -> checkTransfersMetadata(0, 2,
-                        testData.getDAppAddress(),
-                        WAVES_STRING_ID,
-                        testData.getWavesAmount().value()),
-                () -> checkTransfersMetadata(0, 3,
-                        testData.getCallerAddress(),
-                        WAVES_STRING_ID,
-                        testData.getWavesAmount().value()),
+                () -> checkEthereumInvokeIssueAssetMetadata(txIndex, 0, getIssueAssetData()),
 
-                () -> checkStateUpdateBalance(0,
-                        0,
-                        testData.getCallerAddress(),
-                        WAVES_STRING_ID,
-                        calcBalances.getCallerBalanceWavesBeforeTransaction(),
-                        calcBalances.getCallerBalanceWavesAfterTransaction()), // here
+                () -> checkEthereumTransfersMetadata(txIndex, 0, dAppAddressStr, assetIdStr, assetPayment),
+                () -> checkEthereumTransfersMetadata(txIndex, 1, dAppAddressStr, null, assetPayment),
+                () -> checkEthereumTransfersMetadata(txIndex, 2, dAppAddressStr, WAVES_STRING_ID, wavesPayment),
+                () -> checkEthereumTransfersMetadata(txIndex, 3, senderAddressStr, WAVES_STRING_ID, wavesPayment),
 
-                () -> checkStateUpdateBalance(0,
-                        1,
-                        testData.getAssetDAppAddress(),
-                        WAVES_STRING_ID,
-                        calcBalances.getDAppBalanceWavesBeforeTransaction(),
-                        calcBalances.getDAppBalanceWavesAfterTransaction()), // here 2
-                () -> checkStateUpdateBalance(0,
-                        2,
-                        testData.getAssetDAppAddress(),
-                        null,
-                        0, dAppAssetAmountAfter),
-                () -> checkStateUpdateBalance(0,
-                        3,
-                        testData.getAssetDAppAddress(),
-                        assetIdToStr,
-                        calcBalances.getDAppBalanceIssuedAssetsBeforeTransaction(),
-                        calcBalances.getDAppBalanceIssuedAssetsAfterTransaction()),
+                () -> checkStateUpdateBalance(txIndex, 0, senderAddressStr, WAVES_STRING_ID, senderBalanceWavesBeforeTx, senderBalanceWavesAfterTx),
+                () -> checkStateUpdateBalance(txIndex, 1, senderAddressStr, assetIdStr,  senderAssetBalanceBeforeTx, senderAssetBalanceAfterTx),
 
-                () -> checkStateUpdateBalance(0,
-                        4,
-                        testData.getDAppAddress(),
-                        WAVES_STRING_ID,
-                        calcBalances.getAccBalanceWavesBeforeTransaction(),
-                        calcBalances.getAccBalanceWavesAfterTransaction()),
-                () -> checkStateUpdateBalance(0,
-                        5,
-                        testData.getDAppAddress(),
-                        assetIdToStr,
-                        calcBalances.getAccBalanceIssuedAssetsBeforeTransaction(),
-                        calcBalances.getAccBalanceIssuedAssetsAfterTransaction()),
-                () -> checkStateUpdateBalance(0,
-                        6,
-                        testData.getDAppAddress(),
-                        null,
-                        0, recipientAmountValueAfter),
+                () -> checkStateUpdateBalance(txIndex, 2, assetDAppAddressStr, WAVES_STRING_ID, wavesAccDAppBalanceBeforeTx, wavesAccDAppBalanceAfterTx),
+                () -> checkStateUpdateBalance(txIndex, 3, assetDAppAddressStr, null, 0, dAppAssetAmountAfter),
+                () -> checkStateUpdateBalance(txIndex, 4, dAppAddressStr, WAVES_STRING_ID, wavesDAppBalanceBeforeTx, wavesDAppBalanceAfterTx),
+                () -> checkStateUpdateBalance(txIndex, 5, dAppAddressStr, assetIdStr, dAppAssetBalanceBeforeTx, dAppAssetBalanceAfterTx),
+                () -> checkStateUpdateBalance(txIndex, 6, dAppAddressStr, null, 0, assetPayment),
 
-                () -> checkStateUpdateAssets(0, 0, getIssueAssetData(), getIssueAssetVolume())
+                () -> checkStateUpdateAssets(txIndex, 0, getIssueAssetData(), getIssueAssetVolume())
         );
     }
 }
